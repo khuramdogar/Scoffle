10b2c81b4364a7fa4d929661e391fa02a81bf7fe
Khuram Rd <Khuramdogar73@gmail.com>
2018-11-27 20:42:41 +0500

    implemented menu items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :categories
+    resources :categories do
+      resources :items, except:[:index]
+    end
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_131148) do
+ActiveRecord::Schema.define(version: 2018_11_27_135618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2018_11_27_131148) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
+  add_foreign_key "items", "categories"
 end
